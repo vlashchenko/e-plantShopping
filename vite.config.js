7d5ec9ec4bca4f01b4bafb2a2aba0b2506829e33
Your NameSergii <s.vlashchenko@gmail.com>
2024-07-13 20:56:47 -0700

    fixes for vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shoppingreact",
   plugins: [react()],
-})
+  base: "/shoppingreact/",
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: "index.html",
+    },
+  },
+});
