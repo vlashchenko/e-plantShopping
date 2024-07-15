46afd963889358b9f3a6561b837e24b61bc976fc
Your NameSergii <s.vlashchenko@gmail.com>
2024-07-14 21:36:57 -0700

    8

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,22 @@
 // e-plantShopping/vite.config.js
 
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig(({ mode }) => ({
+//   plugins: [react()],
+//   base: mode === 'development' ? '/' : '/shoppingreact/',
+//   build: {
+//     outDir: 'dist',
+//   },
+// }));
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: mode === 'development' ? '/' : '/shoppingreact/',
+  base: '/',
   build: {
     outDir: 'dist',
   },
